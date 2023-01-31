6d578df2e5599f36252215a6e3fef2fc686c6ece
Behruz Aminboyev <behruzaminboyev01@gmail.com>
2023-01-31 19:19:08 +0300

    Kutubxona Sayti Api lar bilan yaratilgan

---
+++ Kutubxona Sayti Api/books/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    subtitle = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    isbn = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
